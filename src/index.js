9d02c61ad91ea152663584e7dc4e470f4b54fda3
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-08-02 22:53:43 +0100

    basic server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("hi");
+// import express
+const express = require("express");
+
+// set up port
+const PORT = process.env.PORT || 4000;
+
+// new instance of app created
+const app = express();
+
+// fn to create connection and listen to port
+const init = async () => {
+  try {
+    // connect to DB
+    await connection.sync({ force: false });
+
+    // server listen on PORT
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to start server | ${error.message}`);
+  }
+};
+
+init();
